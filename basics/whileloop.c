f0545ebed9d7d76bd5714f8f615429f4c50fab98
Patrick Burris <patrick@briefmedia.com>
2017-02-07 22:07:37 -0600

    add whileloop example

---
+++ basics/whileloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+  int i = 10;
+
+  // While loop example
+  while (i < 100) {
+    printf("The number is: %d\n", i);
+    i += 10;
+  }
+
+  return 0;
+}

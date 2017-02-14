dab747c8860280fc10a649ffdc55a2bf62f4f34d
Patrick Burris <patrick@briefmedia.com>
2017-02-13 19:51:45 -0600

    mpc is parsing number calculations

--- mpc/parsing.c
+++ mpc/parsing.c
@@ -1,36 +1,65 @@
-#include <stdio.h>
-#include <string.h>
+#include "mpc.h"
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
 
-  /* Print Version and Exit Information */
-  puts("PScript Version a0.0.1");
+  /* Create Some Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy    = mpc_new("lispy");
+
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                            \
+      number   : /-?[0.0-9.0]+/ ;                                \
+      operator : '+' | '-' | '*' | '/' ;                         \
+      expr     : <number> | '(' <expr> <operator> <expr>+ ')' ;  \
+      lispy    : /^/ <expr> <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lispy);
+
+  puts("Lispy Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
 
-  /* Main loop controller */
-  int running = 1;
+  while (1) {
 
-  /* In a never ending loop */
-  while (running) {
+    char* input = readline("lispy> ");
+    add_history(input);
 
-    /* Output our prompt */
-    fputs("PScript> ", stdout);
-
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
-
-    if (!strncmp(input, "exit", 4)) {
-      running = 0;
+    /* Attempt to parse the user input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      /* On success print and delete the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
     } else {
-      /* Echo input back to user */
-      printf("%s\n", input);
+      /* Otherwise print and delete the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
     }
+
+    free(input);
   }
 
-  fputs("Goodbye!\n", stdout);
-
+  /* Undefine and delete our parsers */
   return 0;
 }
